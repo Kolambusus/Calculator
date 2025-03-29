b9ca80373e345a85a706d2627d5a0887340ef73d
Kolambus <kolambulus@gmail.com>
2025-03-29 18:59:48 +0300

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,62 @@
+# Самый простой калькулятор на Python
+# 
+# v. 1.0
+#
+# Сделано Kolambus
+#
+
+def calculator():
+
+    operation = ['+', '-', '/', '*', '//', '%']
+    print('Привет, я калькулятор!')
+    print('')
+    number_1 = int(input('Введите первое число: '))
+    number_2 = int(input('Введите второе число: '))
+    print('')
+    print('Выберите операцию - ', operation)
+    operat = input()
+
+    if operat == '+':
+        result = number_1 + number_2
+        print(result)
+    elif operat == '-':
+        result = number_1 - number_2
+        print(result)
+    elif operat == '/':
+        if number_1 != 0 and number_2 != 0:
+            result = number_1 / number_2
+            print(result)
+        else:
+            print('[Ошибка] На ноль делять нельзя!')
+            print('Попробуйте еще раз :)')
+            return calculator()
+    elif operat == '*':
+        result = number_1 * number_2
+        print(result)
+    elif operat == '//':
+        if number_1 != 0 and number_2 != 0:
+            result = number_1 // number_2
+            print(result)
+        else:
+            print('[Ошибка] На ноль делять нельзя!')
+            print('Попробуйте еще раз :)')
+            return calculator()
+    elif operat == '%':
+        if number_1 != 0 and number_2 != 0:
+            result = number_1 % number_2
+            print(result)
+        else:
+            print('[Ошибка] На ноль делять нельзя!')
+            print('Попробуйте еще раз :)')
+            return calculator()
+    else:
+        print('[Ошибка] Нужно было выбрать операцию!')
+        print('Попробуйте еще раз :)')
+        return calculator()
+    
+calculator()
+
+
+
+
+
